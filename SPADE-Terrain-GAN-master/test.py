622321daef2beb6a5ef3ecf6f91af1fab3e39fb8
Yao Chen <u7722352@anu.edu.au>
2024-05-01 20:34:52 +1000

    test

--- SPADE-Terrain-GAN-master/test.py
+++ SPADE-Terrain-GAN-master/test.py
@@ -1,4 +1,7 @@
 import os
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 import tensorflow as tf
-print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))\
+
+
+
